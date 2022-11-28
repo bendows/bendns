6c9e094730339a323a748b2f667be17c448be828
bendows <ben@updated.co.za>
2022-11-28 10:40:04 +0200

    working

--- main.go
+++ main.go
@@ -42,15 +42,18 @@ func main() {
 	dns.HandleFunc(".", func(w dns.ResponseWriter, req *dns.Msg) {
 		m := new(dns.Msg)
 		m.SetReply(req)
+
 		for _, q := range req.Question {
+
 			log.Printf("[%s] %v %v\n", w.RemoteAddr(), q.Name, q)
-			if strings.HasSuffix(q.Name, "ben.co.za.") {
+
+			if strings.HasSuffix(q.Name, "fblks.io.") {
 
 				switch q.Qtype {
 				case dns.TypeANY, dns.TypeA, dns.TypeAAAA:
 					m.Authoritative = true
 					m.Answer = append(m.Answer, &dns.A{
-						A: net.ParseIP("1.2.3.4"),
+						A: net.ParseIP(*listenIP),
 						Hdr: dns.RR_Header{
 							Name:   q.Name,
 							Rrtype: dns.TypeA,
@@ -64,7 +67,7 @@ func main() {
 					log.Printf("request %v is MX question\n", req.Question)
 					m.Answer = append(m.Answer, &dns.MX{
 						Preference: 10,
-						Mx:         "mail.ben.co.za.",
+						Mx:         "mail1.fblks.io.",
 						Hdr: dns.RR_Header{
 							Name:   q.Name,
 							Rrtype: dns.TypeMX,
@@ -73,7 +76,7 @@ func main() {
 					})
 					m.Answer = append(m.Answer, &dns.MX{
 						Preference: 15,
-						Mx:         "mail2.ben.co.za.",
+						Mx:         "mail2.fblks.io.",
 						Hdr: dns.RR_Header{
 							Name:   q.Name,
 							Rrtype: dns.TypeMX,
@@ -85,7 +88,7 @@ func main() {
 					m.Authoritative = true
 					log.Printf("request %v is NS question\n", req.Question)
 					m.Answer = append(m.Answer, &dns.NS{
-						Ns: "ns1.ben.co.za.",
+						Ns: "ns1.fblks.io.",
 						Hdr: dns.RR_Header{
 							Name:   q.Name,
 							Rrtype: dns.TypeNS,
@@ -93,7 +96,7 @@ func main() {
 						},
 					})
 					m.Answer = append(m.Answer, &dns.NS{
-						Ns: "ns2.ben.co.za.",
+						Ns: "ns2.fblks.io.",
 						Hdr: dns.RR_Header{
 							Name:   q.Name,
 							Rrtype: dns.TypeNS,
@@ -108,6 +111,7 @@ func main() {
 					// 	log.Printf("request %v is SOA question\n", req.Question)
 					// 	break
 				}
+				log.Printf("Answer %v\n", m)
 				return
 			}
 		}
